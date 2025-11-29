6984d61821719f6d5f5c312f6b560e3b2d00d61e
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-29 21:09:12 +0530

    Time: 2 ms (100%), Space: 51.8 MB (27.61%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,38 +1,25 @@
 class Solution {
-    public int[] findRightInterval(final int[][] intervals) {
-        final int n = intervals.length;
-        final int[][] starts = new int[n][2];
-
-        for(int i = 0; i < n; ++i) {
-            starts[i][0] = intervals[i][0];
-            starts[i][1] = i;
+    public int[] findRightInterval(int[][] intervals) {
+        int n = intervals.length;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int[] intr : intervals) {
+            if (max < intr[0]) max = intr[0];
+            if (min > intr[0]) min = intr[0];
         }
-
-        Arrays.sort(starts, (a, b) -> Integer.compare(a[0], b[0]));
-
-        final int[] res = new int[n];
-
-        for(int i = 0; i < n; ++i) {
-            final int end = intervals[i][1], idx = binarySearch(starts, end);
-            
-            res[i] = (idx == n) ? -1 : starts[idx][1];
+        int m = max - min + 1;
+        int[] startIdx = new int[m];
+        for (int i = 0; i < n; i++) startIdx[intervals[i][0] - min] = i + 1;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            int end = intervals[i][1];
+            if (end > max) {
+                res[i] = -1;
+                continue;
+            }
+            while (startIdx[end - min] == 0) end++;
+            res[i] = startIdx[end - min] - 1;
         }
-
         return res;
-    }
-
-    private int binarySearch(int[][] starts, int target) {
-        int left = 0, right = starts.length;
-
-        while(left < right) {
-            final int mid = left + (right - left) / 2;
-
-            if(starts[mid][0] >= target)
-                right = mid;
-            else
-                left = mid + 1;
-        }
-
-        return left;
     }
 }
