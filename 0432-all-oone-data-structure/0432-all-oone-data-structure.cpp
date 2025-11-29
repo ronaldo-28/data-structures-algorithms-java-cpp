409bf365a827cfe84e872d1425fb85138e662537
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-29 21:06:34 +0530

    Time: 43 ms (99.66%), Space: 76.9 MB (98.22%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,46 +1,57 @@
 class AllOne {
+    unordered_map<string,pair<int,list<string>::iterator>> mp;
+    list<string> l;
 public:
-unordered_map<string,int>mp;
     AllOne() {
-        
     }
     
     void inc(string key) {
-        mp[key]++;
+        if(mp.find(key)==mp.end()){
+            l.push_front(key);
+            mp[key].second=l.begin();
+        }
+        mp[key].first++;
+
+        if(mp[key].first>=mp[l.back()].first){
+            l.splice(l.end(),l,mp[key].second);
+        }
+        else{
+            auto it=mp[key].second;
+            it++;
+            if(it!=l.end() && mp[key].first>mp[*it].first){
+                l.splice(++it,l,mp[key].second);
+            }
+        } 
     }
     
     void dec(string key) {
-         mp[key]--;
-         if(mp[key]==0)
-         mp.erase(key);
+        mp[key].first--;
+        if(mp[key].first<=mp[l.front()].first)
+            l.splice(l.begin(),l,mp[key].second);
+
+        if(mp[key].first==0){
+            mp.erase(key);
+            l.pop_front();
+        }
+        else if(mp.size()>1){
+            auto it=mp[key].second;
+            if(it!=l.begin()){
+                it--;
+                if(mp[key].first<mp[*it].first){
+                    l.splice(--it,l,mp[key].second);
+                }
+            }
+        } 
     }
     
     string getMaxKey() {
-        int maxV=0;
-        string str="";
-        for(auto& it:mp)
-        {
-            if(it.second>maxV)
-            {str=it.first;
-             maxV=it.second;}
-        }
-        return str;
-
+        if(l.empty()) return "";
+        return l.back();
     }
     
     string getMinKey() {
-        int minV=INT_MAX;
-        string str="";
-        for(auto& it:mp)
-        {
-            if(it.second<minV)
-            {
-                 str=it.first;
-                 minV=it.second;
-            }
-            
-        }
-        return str;
+        if(l.empty()) return "";
+        return l.front();
     }
 };
 
