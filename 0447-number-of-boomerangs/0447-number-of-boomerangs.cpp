93ef88192beb79c1a0fa558ce87f413b308e1ff7
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-11-30 23:13:21 +0530

    Time: 67 ms (100%), Space: 12.9 MB (98.13%) - LeetHub

--- 0447-number-of-boomerangs/0447-number-of-boomerangs.cpp
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.cpp
@@ -1,16 +1,56 @@
 class Solution {
 public:
-    int numberOfBoomerangs(vector<vector<int>>& p) {
-        int result = 0;
-        unordered_map<int, int> umap;
-        for(int i=0; i<p.size() ; i++){
-            for(int j=0 ; j<p.size() ; j++){
-                int d = pow(p[j][1] - p[i][1], 2) + pow(p[j][0] - p[i][0], 2);
-                result += 2 * umap[d]++;
-            }
-            umap.clear();
-        }
-        return result;
-    }
+    int dis[501][501];  // Our distances hashmap 
     
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        int n = points.size ();
+        
+        memset(dis, -1, sizeof(dis[0])*points.size()+1);
+		// Initialize all the distances in our array map
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int dist = my_distance(points, i, j);   // my_distance to not clash with std::distance. Also, avoids the vector clutter here.
+                dis [i][j] = dist;
+                dis [j][i] = dist;
+            }
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            // take out the array of dists from point i to the rest
+            auto &len = dis[i];
+            // Sort it so that we can make windows to get our counts
+            ranges::sort(len, len+n);
+            int last = len[0], count = 1;
+            for (int j = 0; j < n; j++)
+            {
+                // ignore garbage values (dist(i,i) in this case)
+                if (len[j] == -1) {
+                    res += count * (count - 1);
+                    count = 1;
+                    continue;
+                }
+                // if the element is the same as the prev, increase window size
+                if (len[j] == last)
+                    count++;
+                else {  // Else, find the final count and start another window size count
+                    res += count * (count - 1);
+                    count = 1;
+                    last = len[j];
+                }
+            }
+            // take care of the case in which the window ends with the array, so we need to calculate boomarang counts after the loop
+            res += count * (count - 1);
+        }
+        // Return the counts of the boomerangs :)
+        return res;
+    }
+	
+    // The inline makes the function feel faster ;)
+    inline int my_distance(vector<vector<int>>& points, int i, int j)
+    {
+        vector<int>& a = points[i];
+        vector<int>& b = points[j];
+        return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);   // returning a clean int
+    }
 };
