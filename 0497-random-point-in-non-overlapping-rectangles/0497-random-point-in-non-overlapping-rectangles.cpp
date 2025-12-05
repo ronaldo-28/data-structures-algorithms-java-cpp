024d642081989cec441855e01eaf27455a52d084
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-12-05 23:14:42 +0530

    Time: 4 ms (94.91%), Space: 73.7 MB (70.57%) - LeetHub

--- 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
+++ 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
-    vector<int> v;
-    vector<vector<int>> rects;
-    // I add the +1 here because in some inputs they contain lines also like 
-	// [ 1,2,1,3 ] ( rectangle with height 0 or width 0 but this also contains 2 points )
-	// to also add these points I add +1.
-    int area(vector<int>& r) {
-        return (r[2] - r[0] + 1) * (r[3] - r[1] + 1);
-    }
-    
-    Solution(vector<vector<int>> rect) {
-        rects = rect;
-        int totalArea=0;
-        for (auto r: rects) {
-            totalArea+=area(r);
-            v.push_back(totalArea);
+    vector<vector<int>> r;
+    vector<int> pre;
+    int total;
+    Solution(vector<vector<int>>& v) {
+        r=v;
+        int tot=0;
+        vector<int> a;
+        for(auto it:v){
+            tot+=(it[2]-it[0]+1)*(it[3]-it[1]+1);
+            a.push_back((it[2]-it[0]+1)*(it[3]-it[1]+1));
         }
+        //total area:[0,tot)
+        for(int i=1;i<a.size();i++){
+            a[i]+=a[i-1];
+        }
+        total=tot;
+        pre=a;
     }
     
     vector<int> pick() {
-        // pick a random reactangle in rects
-        int rnd = rand() % v.back();
-        int idx = upper_bound(v.begin(), v.end(), rnd) - v.begin();
+        int num=rand()%total;
+
+        auto ind=upper_bound(pre.begin(),pre.end(),num)-pre.begin();
         
-        // pick a random point in rects[idx]
-        auto r = rects[idx];
-        int x = rand() % (r[2] - r[0] + 1) + r[0];
-        int y = rand() % (r[3] - r[1] + 1) + r[1];
-        return { x, y };
+        int x1=r[ind][0],y1=r[ind][1],x2=r[ind][2],y2=r[ind][3];
+        int lenX=x2-x1+1;
+        int xRand=rand()%lenX;
+        int chooseX=x1+xRand;
+
+        int lenY=y2-y1+1;
+        int yRand=rand()%lenY;
+        int chooseY=y1+yRand;
+        return {chooseX,chooseY};
     }
 };
+// 0,1,2,3
+
+// -2, 1
+
+
+// 3
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(rects);
+ * vector<int> param_1 = obj->pick();
+ */
+
+
+// 30 mins for each
