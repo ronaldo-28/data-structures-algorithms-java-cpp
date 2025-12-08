02a96cd7bd87882e81869bd0df28603637954ed6
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2025-12-08 21:08:24 +0530

    Time: 3 ms (100%), Space: 61.3 MB (60.72%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[2*n+1];
+        for (int i = 0; i < f.length; i++) {
+            f[i] = -2;
+        }
+        f[n] = -1;
+
+        int maxlen = 0, prefixSum = n;
+        for (int i = 0; i < n; i++) {
+            prefixSum += nums[i] * 2 - 1;
+            if (f[prefixSum] == -2) {
+                f[prefixSum] = i;
+            } else {
+                maxlen = Math.max(maxlen, i - f[prefixSum]);
+            }
+        }
+
+        return maxlen;
+    }
+}
