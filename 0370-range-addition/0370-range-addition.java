f1ad06478b1974f767fe4756e327f42c3488350d
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-08-18 14:05:32 +0530

    Time: 2 ms (100%), Space: 48.9 MB (52.42%) - LeetHub

---
+++ 0370-range-addition/0370-range-addition.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        
+        int[] prefixSum = new int[length];
+        
+        for (int[] update : updates) {
+            prefixSum[update[0]] += update[2];
+            if (update[1] + 1 < length) {
+                prefixSum[update[1] + 1] -= update[2];
+            }
+        }
+        
+        for (int i = 1;  i < length; i++) {
+            prefixSum[i] += prefixSum[i - 1];
+        }
+        
+        return prefixSum;
+    }
+}
