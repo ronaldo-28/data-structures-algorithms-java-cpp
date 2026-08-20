6461cf6846e8acfa420d282a539977a098685230
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-08-20 16:07:21 +0530

    Time: 493 ms (36.03%), Space: 0B (100%) - LeetHub

---
+++ 0579-find-cumulative-salary-of-an-employee/0579-find-cumulative-salary-of-an-employee.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    id,
+    month,
+    SUM(salary) OVER (PARTITION BY id ORDER BY month RANGE BETWEEN 2 PRECEDING AND CURRENT ROW) AS Salary
+FROM Employee
+WHERE (id, month) NOT IN (SELECT id, MAX(month) AS month FROM Employee GROUP BY id)
+ORDER BY id, month DESC
