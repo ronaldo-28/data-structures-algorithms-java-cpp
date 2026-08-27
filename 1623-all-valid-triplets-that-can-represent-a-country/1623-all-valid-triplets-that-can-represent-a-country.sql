50fab756e369d78f7d059e10615c464c41647aa9
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-08-27 20:01:30 +0530

    Time: 3665 ms (5.71%), Space: 0B (100%) - LeetHub

---
+++ 1623-all-valid-triplets-that-can-represent-a-country/1623-all-valid-triplets-that-can-represent-a-country.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT
+    a.student_name AS member_A,
+    b.student_name AS member_B,
+    c.student_name AS member_C
+FROM schoolA a
+JOIN schoolB b 
+    ON a.student_id <> b.student_id 
+        AND a.student_name <> b.student_name
+JOIN schoolC c 
+    ON a.student_id <> c.student_id 
+        AND a.student_name <> c.student_name
+        AND b.student_id <> c.student_id 
+        AND b.student_name <> c.student_name
