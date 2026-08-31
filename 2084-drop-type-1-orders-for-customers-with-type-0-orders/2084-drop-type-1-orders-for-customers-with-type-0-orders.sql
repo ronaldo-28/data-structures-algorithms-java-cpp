64c76fee149f1f9d5b2b0a96d092e3117623e539
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-08-31 12:44:04 +0530

    Time: 455 ms (63.57%), Space: 0B (100%) - LeetHub

---
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT *
+FROM Orders
+WHERE order_type = 0
+
+UNION
+
+SELECT *
+FROM Orders
+WHERE order_type = 1
+    AND customer_id NOT IN (
+        SELECT customer_id
+        FROM Orders
+        WHERE order_type = 0
+);
