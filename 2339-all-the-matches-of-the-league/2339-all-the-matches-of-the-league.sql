da41f37334eedf68b0aa5628c34ed905d6cb7bf9
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-09-02 15:05:14 +0530

    Time: 1070 ms (5.41%), Space: 0B (100%) - LeetHub

---
+++ 2339-all-the-matches-of-the-league/2339-all-the-matches-of-the-league.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select
+    t1.team_name as home_team, 
+    t2.team_name as away_team 
+from teams as t1
+join teams as t2
+on t1.team_name != t2.team_name
