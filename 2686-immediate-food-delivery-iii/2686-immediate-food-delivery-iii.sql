c813749a00bf2617a33174957a3e3f66170e989a
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-09-04 12:33:05 +0530

    Time: 737 ms (26%), Space: 0B (100%) - LeetHub

---
+++ 2686-immediate-food-delivery-iii/2686-immediate-food-delivery-iii.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    order_date,
+    ROUND(
+        SUM(
+            CASE
+                WHEN order_date = customer_pref_delivery_date
+                THEN 1 ELSE 0
+            END
+        ) * 100
+        / COUNT(*),
+    2) AS immediate_percentage
+FROM Delivery
+GROUP BY 1
+ORDER BY 1;
