63bd9f293102af80edfa4f3a230257ec8f963d59
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-09-05 17:07:20 +0530

    Time: 56 ms (60%), Space: 57.8 MB (33.33%) - LeetHub

---
+++ 2777-date-range-generator/2777-date-range-generator.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {number} step
+ * @yields {string}
+ */
+function* dateRangeGenerator(start, end, step) {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  
+  // Convert step to milliseconds
+  const stepMs = step * 24 * 60 * 60 * 1000;
+  
+  let currentDate = startDate;
+  while (currentDate <= endDate) {
+    yield currentDate.toISOString().split('T')[0];
+    currentDate = new Date(currentDate.getTime() + stepMs);
+  }
+}
+
+/**
+ * const g = dateRangeGenerator('2023-04-01', '2023-04-04', 1);
+ * g.next().value; // '2023-04-01'
+ * g.next().value; // '2023-04-02'
+ * g.next().value; // '2023-04-03'
+ * g.next().value; // '2023-04-04'
+ * g.next().done; // true
+ */
