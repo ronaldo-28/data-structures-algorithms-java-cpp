40fb4638ac91a7a5c8ca6192ae27d2da2896f822
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-09-05 12:23:42 +0530

    Delete 2739-total-distance-traveled directory

--- 2739-total-distance-traveled/2739-total-distance-traveled.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int distanceTraveled(int mainTank, int additionalTank) {
-        int extra = Math.min((mainTank - 1) / 4, additionalTank);  
-
-        return (extra + mainTank) * 10;
-    }
-}
