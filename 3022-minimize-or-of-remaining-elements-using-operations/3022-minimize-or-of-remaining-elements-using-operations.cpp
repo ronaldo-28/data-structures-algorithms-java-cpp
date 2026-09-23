effec674f442e30960793991863d0bbda92be77e
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-09-23 12:33:03 +0530

    Time: 20 ms (97.92%), Space: 73.6 MB (54.17%) - LeetHub

--- 3022-minimize-or-of-remaining-elements-using-operations/3022-minimize-or-of-remaining-elements-using-operations.cpp
+++ 3022-minimize-or-of-remaining-elements-using-operations/3022-minimize-or-of-remaining-elements-using-operations.cpp
@@ -1,51 +1,22 @@
 class Solution {
-    
-    int MinOperations (int target, const vector<int>& nums, const vector<bool>& taken) {
-        int n = nums.size();
-        int ans = 0;
-        
-        for (int j = 0; j < n;) {
-            if (!taken[j]) {
-                j ++;
-                continue;
-            }
-            
-            int st = j;
-            int all = nums[j];
-            
-            while (j < n && taken[j] && (all & target) != 0) {
-                all &= nums[j];
-                j ++;
-            }
-            
-            if ((all & target) == 0) ans += (j - st - 1);
-            else ans += (j - st);
-        }
-        return ans;
-    }
-    
 public:
     int minOrAfterOperations(vector<int>& nums, int k) {
-        int n = nums.size();
-        
-        int rev_ans = 0;
-        int ans = 0;
-        
-        for (int bit = 29; bit >= 0; bit --) {
-            int rev_ans_with_cur_bit = rev_ans | (1 << bit);
-            
-            vector<bool> taken(n, false);
-            for (int i = 0; i < n; i ++) {
-                if ((nums[i] & rev_ans_with_cur_bit) == 0) continue;
-                taken[i] = true;
+        int res = 0, mask = 0;
+
+        for (int i = 29; i >= 0; i--) {
+            int next = mask | 1 << i;
+            int sum = 0, cur = next;
+
+            for (auto& c : nums) {
+                cur &= c;
+                sum += !cur;
+                if (!cur) cur = next;
             }
-            
-            if (MinOperations(rev_ans_with_cur_bit, nums, taken) <= k)
-                rev_ans = rev_ans_with_cur_bit;
-            else 
-                ans |= (1 << bit);
+
+            if (nums.size() - sum <= k) mask = next;
+            else res |= 1 << i;
         }
-        
-        return ans;
+
+        return res;
     }
 };
