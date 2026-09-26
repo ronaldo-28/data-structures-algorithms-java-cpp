f267dab40d707b13603d5b4e1023b43314145301
Ronaldo Fernandes <mafronfernandes28@gmail.com>
2026-09-26 12:07:13 +0530

    Time: 35 ms (100%), Space: 46.9 MB (75.81%) - LeetHub

--- 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.java
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.java
@@ -1,70 +1,38 @@
-
 class Solution {
-    record State(int target, int cost) {}; 
+    static int INF = (int)1e9;
     public int minOperations(int[] nums, int sum) {
-        // replace with 2 * x or x / 2 
-        // all multiplication before division 
-        // min no of ops needed so that some subset has sum exactly equal to sum. 
 
+        int n = nums.length;
 
-        // n = 100, sum = 5000, nums[i] = 500 
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, INF);
 
-        // for each position, try to perform all ops, first will be 2*x then simply x/2 and so on... And check what som we can get with all these untill 5000 
+        dp[0] = 0;
 
-        // this is knapsack style DP 
-        // we see at each position what can be the value after applying ops and what will be cost associated with it. 
+        for(int i = 0; i < n; i++){
+            int x = nums[i];
+            
+            int[] nextdp = dp.clone();
+            for(int s = 0; s <= sum; s++){
+                if(dp[s] == INF) continue;
 
-
-        // since we only care about prev col only, we can have 1-D dp 
-        int n = nums.length; 
-        int dp[] = new int[sum + 1]; 
-        // Initially no of steps for each sum will be inf 
-        int inf = Integer.MAX_VALUE; 
-        Arrays.fill(dp, inf); 
-        dp[0] = 0; // base case
-
-        for(int i = 0; i < n; i++) {
-            int cur = nums[i]; 
-            List<State> cost = new ArrayList<>(); // we find cost of each valid value that can be formed
-
-            int ops = 0; 
-            // multiply first 
-            while(cur <= sum) {
-                cost.add(new State(cur, ops)); 
-                cur = cur << 1; // cur = 2 * cur 
-                ops++; 
+                int operations = dp[s];
+              
+                for(int v = x, divide = 0; v > 0; v >>= 1, divide++){
+                    if(s + v <= sum)
+                        nextdp[s + v] = Math.min(nextdp[s + v], operations + divide);
+                }                
+               
+                for(int v = x, multiple = 0; s + v <= sum; v <<= 1, multiple++){
+                    nextdp[s + v] = Math.min(nextdp[s + v], operations + multiple);
+                }                
             }
-
-            cur = nums[i]; 
-            // division ops, we can simply do division on nums[i] - as 2 * x/ 2 = x so ops will increase only for a sum 
-            // assume 2 -> 4 -> 8 -> 4 again after division so for 4, ops increase only if we came back hence either we multiply or divide. 
-            ops = 0; 
-            while(cur >= 1) {
-                if(cur <= sum) cost.add(new State(cur, ops)); 
-                cur = cur >> 1; // cur = cur / 2 
-                ops++; 
-            }
-
-            // Now for all numbers that is reachable using them what sum can I form?? 
-
-            int next[] = dp.clone();  
-
-            for(int s = 0; s <= sum; s++) {
-                if(dp[s] == inf) continue; // s sum can not be formed 
-
-                // else for this sum, iterate over each sum's for ops on current no. 
-
-                for(State c: cost) {
-                    int tar = c.target; 
-                    ops = c.cost; 
-
-                    if(s + tar <= sum) next[s + tar] = Math.min(next[s + tar], dp[s] + ops); 
-                }
-            }
-            dp = next.clone(); 
+            
+            dp = nextdp;
+            if(dp[sum] == 0)
+                return 0;
         }
 
-        return dp[sum] == inf ? -1 : dp[sum]; 
-
+        return dp[sum] < INF? dp[sum] : -1;      
     }
 }
